113151cc754c07d9e56be3c8540245134f39629b
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-01 22:42:09 +0530

    [Atishay Garg] Added Csharp Programs

---
+++ core-csharp-practice/scenario-based/CafeteriaFoodMenu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.scenariobased
+{
+    internal class CafeteriaFoodMenu
+    {
+        static void Main(string[] args)
+        {
+            string[] menuItems =
+            {
+            "Idli",
+            "Dosa",
+            "Vada",
+            "Upma",
+            "Poha",
+            "Fried Rice",
+            "Veg Curry",
+            "Chapati",
+            "Tea",
+            "Coffee"
+        };
+
+            DisplayMenu(menuItems);
+
+            Console.Write("\nEnter the item index you want to order: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            string selectedItem = GetItemByIndex(menuItems, index);
+
+            if (selectedItem != null)
+            {
+                Console.WriteLine("You have ordered: " + selectedItem);
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection. Please choose a valid index.");
+            }
+        }
+
+        // Displays the cafeteria menu
+        static void DisplayMenu(string[] items)
+        {
+            Console.WriteLine("Campus Cafeteria Menu:");
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(i + ". " + items[i]);
+            }
+        }
+
+        // Returns item based on index
+        static string GetItemByIndex(string[] items, int index)
+        {
+            if (index >= 0 && index < items.Length)
+            {
+                return items[index];
+            }
+            return null;
+        }
+    }
+}
