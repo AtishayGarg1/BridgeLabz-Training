6983918264558e53d6a8da3149834128e7a22f3e
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-06 16:16:44 +0530

    [Atishay Garg] Added Csharp Scenario Based Programs

---
+++ oops-csharp-practice/scenario-based/vehicle-rental/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.scenariobased.VehicleTrntai
+{
+    internal class Customer
+    {
+        private string name;
+        private string liscenceNumber;
+
+        public Customer(string name, string liscenceNumber)
+        {
+            this.name = name;
+            this.liscenceNumber = liscenceNumber;
+        }
+
+        public string GetCustomerInfo()
+        {
+            return "Name: " + name + "Liscence Number: " + liscenceNumber;
+        }
+    }
+}
