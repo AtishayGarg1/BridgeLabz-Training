dd5e4881a3a4d2ece8d39e079275dff507645f97
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-06 16:46:41 +0530

    [Atishay Garg] Added Csharp Scenario Based Programs

---
+++ oops-csharp-practice/scenario-based/customer-service-call-log-manager/ICallLogManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.customer_service_call_log
+{
+    internal interface ICallLogManager
+    {
+        void AddCallLog(string customerName, string phoneNumber, string message);
+        void ResizeArray();
+        CallLogs[] SearchByKeyword(string keyword);
+        CallLogs[] FilterByTime(DateTime startTime, DateTime endTime);
+        void DisplayLogs(CallLogs[] logs);
+    }
+}
