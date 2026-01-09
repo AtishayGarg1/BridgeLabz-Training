16a7572d12b5707a44aad2861a1dd7e4e3097a17
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-09 15:36:23 +0530

    [Atishay Garg] Added Csharp ATM Dispenser Scenario-1 Programs

---
+++ data-structures-csharp-practice/scenario-based/atm-dispenser/ATMDispenserMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.scenariobased.atmdispenser
+{
+    internal class ATMDispenserMain
+    {
+        static void Main(string[] args)
+        {
+            ATMDispenser aTMDispenser = new ATMDispenser();
+
+            // Scenario 1 - Optimal combo with 500 note
+            int[] notesPresent = new int[] { 2, 5, 10, 20, 50, 100, 200, 500 };
+            Console.Write("Enter Amount: ");
+            int amount = int.Parse(Console.ReadLine());
+            aTMDispenser.Initialize(amount, notesPresent);
+        }
+    }
+}
---
+++ data-structures-csharp-practice/scenario-based/atm-dispenser/ATMNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.scenariobased.atmdispenser
+{
+    // Node class representing a single currency note in the ATM
+    internal class ATMNode
+    {
+        public int Amount;
+        public ATMNode Next;
+
+        public ATMNode(int amount)
+        {
+            Amount = amount;
+            Next = null;
+        }
+
+        // Override ToString() to Display Node
+        public override string? ToString()
+        {
+            return "Amount: " + Amount;
+        }
+    }
+}
