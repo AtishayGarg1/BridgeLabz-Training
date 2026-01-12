00f389dc7bb19626da74ca48db4b17032b169399
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-12 16:19:33 +0530

    [Atishay Garg] Added Scenario Based Csharp Programs

---
+++ oops-csharp-practice/scenario-based/metal-factory-pipe-cutting/IRodCuttingStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.metal_factory_pipe_cutting
+{
+    interface IRodCuttingStrategy
+    {
+        int CalculateOptimalRevenue(int[] priceTable, int length);
+        int CalculateSimpleRevenue(int[] priceTable, int length);
+    }
+}
