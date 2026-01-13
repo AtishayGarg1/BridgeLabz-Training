98641674393f27f336c39d916d401e2f51004944
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-13 16:49:41 +0530

    [Atishay Garg] Added loan-buddy scenario based programs

---
+++ oops-csharp-practice/scenario-based/Loan-Buddy/IApprovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BridgeLabzTraining.Loan_Buddy
+{
+   public  interface IApprovable
+    {
+        bool ApproveLoan();
+        double CalculateEMI();
+    }
+}
---
+++ oops-csharp-practice/scenario-based/Loan-Buddy/LoanMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Loan_Buddy
+{
+    internal class LoanMain
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                LoanMenu menu = new LoanMenu();
+                menu.Start();
+            }
+        }
+    }
+}
