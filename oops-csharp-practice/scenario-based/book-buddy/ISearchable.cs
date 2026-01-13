f1838441aaccbd457b2686faabf016b0fced0fe1
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-13 15:39:52 +0530

    [Atishay Garg] Added book-buddy and cinema-time scenario based programs

---
+++ oops-csharp-practice/scenario-based/book-buddy/ISearchable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.scenariobased.BookBuddy
+{
+    internal interface ISearchable
+    {
+        void AddBook(string bookName, string bookAuthor);
+        void SortBooksAlphabetically();
+        void SearchByAuthor(string bookAuthor);
+        void InitializeBook();
+        void DisplayAllBooks();
+
+
+    }
+}
