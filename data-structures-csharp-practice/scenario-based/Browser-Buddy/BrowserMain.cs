a620986173a52b90d285ab6cc36ede206414b6d2
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-16 16:18:41 +0530

    [Atishay Garg] Updated Browser Buddy Scenario Based Programs

--- data-structures-csharp-practice/scenario-based/Browser-Buddy/BrowserMain.cs
+++ data-structures-csharp-practice/scenario-based/Browser-Buddy/BrowserMain.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BridgeLabzTraining.scenariobased.BrowserBuddy
-{
-    internal class BrowserMain
-    {
-        static void Main(string[] args)
-        {
-            Browser browser = new Browser(5);
+//namespace BridgeLabzTraining.scenariobased.BrowserBuddy
+//{
+//    internal class BrowserMain
+//    {
+//        static void Main(string[] args)
+//        {
+//            Browser browser = new Browser(5);
 
-            Tab tab1 = browser.OpenTab();
-            tab1.History.Visit("google.com");
-            tab1.History.Visit("github.com");
-            tab1.History.Back();
-            tab1.History.Forward();
+//            Tab tab1 = browser.OpenTab();
+//            tab1.History.Visit("google.com");
+//            tab1.History.Visit("github.com");
+//            tab1.History.Back();
+//            tab1.History.Forward();
 
-            browser.CloseTab(0);
+//            browser.CloseTab(0);
 
-            Tab restored = browser.RestoreTab();
-            Console.WriteLine("Current Page: " + restored.History.GetCurrent());
-        }
-    }
-}
+//            Tab restored = browser.RestoreTab();
+//            Console.WriteLine("Current Page: " + restored.History.GetCurrent());
+//        }
+//    }
+//}
