9cdeaf4b0788ff65023328a7a96b8efae35a35cf
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-20 10:45:33 +0530

    [Atishay Garg] Added Exam Proctor Scenario Based Programs

---
+++ data-structures-csharp-practice/scenario-based/exam-proctor/AnswerMap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.DSA_FOLDER.scenariobased.exam_proctor
+{
+    internal class AnswerMap
+    {
+        private int[] keys;      // question IDs
+        private char[] values;   // answers
+        private int size;
+
+        public AnswerMap(int capacity)
+        {
+            keys = new int[capacity];
+            values = new char[capacity];
+            size = 0;
+        }
+
+        public void Put(int questionId, char answer)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (keys[i] == questionId)
+                {
+                    values[i] = answer; // update
+                    return;
+                }
+            }
+
+            keys[size] = questionId;
+            values[size] = answer;
+            size++;
+        }
+
+        public char Get(int questionId)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (keys[i] == questionId)
+                    return values[i];
+            }
+            return '\0'; // not answered
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public int GetKeyAt(int index)
+        {
+            return keys[index];
+        }
+    }
+}
