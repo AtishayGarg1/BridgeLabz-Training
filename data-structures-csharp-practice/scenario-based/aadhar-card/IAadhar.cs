04292242daaec2218758427799d1a7951189986e
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-20 15:07:00 +0530

    [Atishay Garg] Added Aadhar Card Management Scenario Based Programs

---
+++ data-structures-csharp-practice/scenario-based/aadhar-card/IAadhar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.DSA_FOLDER.scenariobased.aadharcard
+{
+    // Interface to Provid Security
+    internal interface IAadhar
+    {
+        void AddAadhar();
+        void SortAadhar();
+        void SearchAadhar();
+        void DisplayAllAadhar();
+    }
+}
