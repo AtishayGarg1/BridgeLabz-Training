87702773f228428b087d306631846b737a9f4303
Atishay Garg <atishaygarg3003@gmail.com>
2026-01-25 22:25:33 +0530

    [Atishay Garg] Added Csharp Leetcode program

---
+++ collections-csharp-practice/leetcode-codebase/MinimumDifferenceBetweenHighestAndLowest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.csharp_collections.leetcode
+{
+    internal class MinimumDifferenceBetweenHighestAndLowest
+    {
+        public int MinimumDifference(int[] nums, int k)
+        {
+            if (k == 1)
+            {
+                return 0;
+            }
+            Array.Sort(nums);
+            int min = int.MaxValue;
+            for (int i = 0; i <= nums.Length - k; i++)
+            {
+                min = Math.Min(min, nums[i - 1 + k] - nums[i]);
+            }
+            return min;
+        }
+    }
+}
