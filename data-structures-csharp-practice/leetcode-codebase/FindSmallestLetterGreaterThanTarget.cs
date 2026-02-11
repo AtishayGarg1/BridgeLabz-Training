d22cabded4cc5e41e1f09041a0340c6f2d85db01
Atishay Garg <atishaygarg3003@gmail.com>
2026-02-11 22:21:32 +0530

    [Atishay Garg] Added CSharp Leetcode Programs

---
+++ data-structures-csharp-practice/leetcode-codebase/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,24 @@
+class FindSmallestLetterGreaterThanTarget
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        int l = 0;
+        int r = letters.Length;
+
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+
+            if (letters[m] > target)
+            {
+                r = m;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+
+        return letters[l % letters.Length];
+    }
+}
